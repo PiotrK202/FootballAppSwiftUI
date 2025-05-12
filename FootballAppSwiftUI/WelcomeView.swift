7558afa8f63d24a4afb82d1081d67ac795376eac
PiotrK <piotr.koscielny69@gmail.com>
2025-05-12 10:19:28 +0200

    created welcomeView

---
+++ FootballAppSwiftUI/WelcomeView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  FootballAppSwiftUI
+//
+//  Created by piotr koscielny on 12/5/25.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        ZStack {
+            Image("welcomeImage")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            VStack {
+                Text("Welcome to Football App")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .padding(.top, 40)
+                    .padding(.horizontal, 15)
+                
+                Spacer()
+                
+                Button(action: {
+                }) {
+                    Text("Start")
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 40)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                        .padding(.horizontal, 24)
+                }
+                .padding(.bottom, 40)
+            }
+        }
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
